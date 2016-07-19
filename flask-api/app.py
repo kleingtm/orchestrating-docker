658d22389159b7d3b3d533c3b580f9afe04b9d5e
Tom Kleingers <tkleingers@evanstonavenue.com>
2016-07-19 07:16:03 -0500

    Current progress

---
+++ flask-api/app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask.ext.pymongo import PyMongo
+from flask.ext.restful import Api, Resource
+from users import user, users
+
+
+# configure flask app, rest api, and mongodb (pymongo)
+app = Flask(__name__)
+api = Api(app)
+mongo = PyMongo(app)
+
+app.register_blueprint(user)  # register route from user.py
+app.register_blueprint(users)  # register route from users.py
+
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
+# The below doesn't work! You must run the app outside of the __name__ == '__main__' block
+if __name__ == '__main__':
+    print(' ')
+    print(' * uWSGI running on :5000/api/user')
+    print(' * uWSGI running on :5000/api/users')
+    print(' ')
+    app.run()
---
+++ flask-api/users.py
@@ -0,0 +1,13 @@
+from flask import Blueprint
+user = Blueprint('user', __name__)
+users = Blueprint('users', __name__)
+
+
+@user.route('/flask-api/user')
+def user_fn():
+    return "Hey single user!"
+
+
+@users.route('/flask-api/users')
+def users_fn():
+    return "Hey multiple users!"
